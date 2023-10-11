19eae89128897a267d51ca4e022b5a620c2fce80
ovinhovini <122178083+ovinhovini@users.noreply.github.com>
2023-10-11 17:20:57 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+function pom(){
+    const pom = document.getElementById("som_tecla_pom")
+    pom.play()
+}
+
+function clap(){
+    const clap = document.getElementById("som_tecla_clap")
+    clap.play()
+}
+
+function tim(){
+    const tim = document.getElementById("som_tecla_tim")
+    tim.play()
+}
+
+function puff(){
+    const puff = document.getElementById("som_tecla_puff")
+    puff.play()
+}
+
+function splash(){
+    const splash = document.getElementById("som_tecla_splash")
+    splash.play()
+}
+
+function toim(){
+    const toim = document.getElementById("som_tecla_toim")
+    toim.play()
+}
+
+function psh(){
+    const psh = document.getElementById("som_tecla_psh")
+    psh.play()
+}
+
+function tic(){
+    const tic = document.getElementById("som_tecla_tic")
+    tic.play()
+}
+
+function tom(){
+    const tom = document.getElementById("som_tecla_tom")
+    tom.play()
+}
